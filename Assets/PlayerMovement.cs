62e0290dad1f2ffa04e656c32a66ed3dd8e99527
ankeris <31132643+ankeris@users.noreply.github.com>
2017-11-06 17:02:33 +0100

    Added game beta

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sideForce = 700f;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // add force to move forward
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        } else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.AddForce(- sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
