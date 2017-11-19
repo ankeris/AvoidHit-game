18895827053db2507ba5897772cc40aac3317d6a
ankeris <31132643+ankeris@users.noreply.github.com>
2017-11-19 23:10:36 +0100

    added scoring

---
+++ Assets/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+
+    public Transform player;
+    public Text scoreText;
+
+	// Update is called once per frame
+	void Update () {
+
+        float smallerNumber = player.position.z / 12;
+
+        string calc = smallerNumber.ToString("0");
+   
+        scoreText.text = calc;
+	}
+}
